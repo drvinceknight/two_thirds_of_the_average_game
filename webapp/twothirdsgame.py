b34349b90599267eae2853c577aed4eaed288d8b
drvinceknight <vincent.knight@gmail.com>
2013-04-23 21:51:00 +0100

    started user form

--- webapp/twothirdsgame.py
+++ webapp/twothirdsgame.py
@@ -1,15 +1,19 @@
 import webapp2
-import django
+import jinja2
+import os
+
+from google.appengine.api import users
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+                                        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, webapp2 World!')
-
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write('Hello, webapp2 World!')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
