71e76ba51f7254271fea8120c6b4af40c6fd8c32
drvinceknight <vincent.knight@gmail.com>
2013-04-24 17:28:24 +0100

    some experiments

--- webapp/twothirdsgame.py
+++ webapp/twothirdsgame.py
@@ -7,13 +7,29 @@ from google.appengine.api import users
 JINJA_ENVIRONMENT = jinja2.Environment(
                                         loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
+        user = users.get_current_user()
         #self.response.headers['Content-Type'] = 'text/plain'
         #self.response.write('Hello, webapp2 World!')
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render())
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            html_file = 'index.html'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            html_file = 'login.html'
+
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext
+        }
+
+        template = JINJA_ENVIRONMENT.get_template(html_file)
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
