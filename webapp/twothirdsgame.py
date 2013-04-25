04f770a360219a48fc7f8f31f3bffb7d798ef1c3
drvinceknight <vincent.knight@gmail.com>
2013-04-25 18:18:14 +0100

    V1.0 of the app now working

--- webapp/twothirdsgame.py
+++ webapp/twothirdsgame.py
@@ -10,9 +10,25 @@ MAIN_PAGE_HTML = """\
   <body>
     <h1>Two thirds of the average game</h1>
 
+    <p>Welcome to this online version of the two thirds of the average game.</p>
+    <p>Here are the rules of the game as given on the <a href="http://en.wikipedia.org/wiki/Guess_2/3_of_the_average">wikipedia page</a>: <blockquote>"In game theory, Guess 2/3 of the average is a game where several people guess what 2/3 of the average of their guesses will be, and where the numbers are restricted to the real numbers between 0 and 100, inclusive. The winner is the one closest to the 2/3 average."</blockquote></p>
+
+    <p>I use this game quite a lot in my teaching and during outreach events. I've blogged about it <a href="http://drvinceknight.blogspot.co.uk/2013/04/two-thirds-of-average-game.html">here</a>.</p>
+
+    <p>It's a game to get participants to understand concepts of dominated strategies:</p>
+
+    <img src="/images/Rationalise_two_thirds_of_average_game.gif" width=500>
+"""
+
+LOGGED_IN_PAGE_HTML = """\
+    <p><b>If you are on this page it is because you are currently logged in via your google user name. I am using this to ensure that every one who chooses to participate will have their last guess only counted (there is an option to logout below).</b></p>
+
+    <p> Please enter a guess below. If you enter a url (G+ page, twitter page, website...) I'll be sure to include it when I disclose the winner(s) on my <a href="http://drvinceknight.blogspot.co.uk/">blog page</a> :)</p>
+
     <form action="/guess" method="post">
         Guess: <input name="guess"><br>
         Name: <input name="name" value="Anonymous"><br>
+        url: <input name="personel_url"><br>
         <input type="submit" value="Submit form">
     </form>
     <a href=%s>%s</a>
@@ -22,8 +38,7 @@ MAIN_PAGE_HTML = """\
 """
 
 LOGGED_OUT_PAGE_HTML = """\
-<html>
-  <body>
+    <p><b>If you are on this page it is because you are currently not logged in. To ensure that the game is played fairly and no one has anymore information than any other player, please log in using your google account.</b></p>
     <a href=%s>%s</a>
   </body>
 </html>
@@ -35,6 +50,7 @@ class Guess(db.Model):
     author = db.StringProperty()
     name = db.StringProperty()
     number = db.StringProperty()
+    personel_url = db.StringProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
 
@@ -57,15 +73,16 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         user = users.get_current_user()
+        page = MAIN_PAGE_HTML
 
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
-            page = MAIN_PAGE_HTML
+            page += LOGGED_IN_PAGE_HTML
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-            page = LOGGED_OUT_PAGE_HTML
+            page += LOGGED_OUT_PAGE_HTML
         self.response.write(page % (url, url_linktext))
 
 
@@ -75,6 +92,7 @@ class Guessed(webapp2.RequestHandler):
 
         name = self.request.get('name')
         number = self.request.get('guess')
+        personel_url = self.request.get('personel_url')
 
         if is_valid_guess(number):
 
@@ -82,6 +100,7 @@ class Guessed(webapp2.RequestHandler):
             guess.author = users.get_current_user().nickname()
             guess.name = name
             guess.number = number
+            guess.personel_url = personel_url
             guess.put()
 
             Q = Guess.all()
